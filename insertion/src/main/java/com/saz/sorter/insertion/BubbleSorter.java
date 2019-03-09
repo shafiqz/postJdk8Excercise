762f64969c24f2c8762ad68ef92a8bd041211eeb
shafiq <shafiq.akram.zakaria@gmail.com>
2019-03-09 22:45:17 +0800

    Check-in bubble sort implementation.

---
+++ insertion/src/main/java/com/saz/sorter/insertion/BubbleSorter.java
@@ -0,0 +1,25 @@
+package com.saz.sorter.bubble;
+
+import com.saz.sorter.api.Sorter;
+
+import java.util.Arrays;
+
+public class BubbleSorter implements Sorter {
+
+    @Override
+    public <T extends Comparable<T>> T[] sort(final T[] input) {
+
+        T[] sorted = Arrays.copyOf(input, input.length);
+
+        for (int i = 0; i < sorted.length; ++i) {
+
+            for (int j = 0; j < sorted.length - 1; ++j) {
+                if (sorted[j].compareTo(sorted[j + 1]) < 0) {
+                    swap(sorted, j, j + 1);
+                }
+            }
+        }
+
+        return sorted;
+    }
+}
---
+++ insertion/src/test/java/com/saz/sorter/bubble/BubbleSorterTest.java
@@ -0,0 +1,34 @@
+package com.saz.sorter.selection;
+
+import com.saz.sorter.api.Sorter;
+import com.saz.sorter.bubble.BubbleSorter;
+
+import java.util.concurrent.ThreadLocalRandom;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BubbleSorterTest {
+
+    private ThreadLocalRandom random = ThreadLocalRandom.current();
+    int arraySize = 100;
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void sort() {
+
+        Integer[] array = new Integer[100];
+        for (int i = 0; i <= array.length - 1; ++i) {
+            array[i] = random.nextInt();
+        }
+
+        Sorter sorter = new BubbleSorter();
+        Integer[] sorted = sorter.sort(array);
+
+        for (int i = 1; i <= sorted.length - 1; ++i) {
+            assertTrue(sorted[i - 1] <= sorted[i],
+                       "Element " + sorted[i - 1] + " is not less than " + sorted[i]);
+        }
+    }
+}
