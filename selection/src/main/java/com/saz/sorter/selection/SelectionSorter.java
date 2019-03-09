551cedce543f548204d6a0d8800eb137abc3ce80
shafiq <shafiq.akram.zakaria@gmail.com>
2019-03-09 13:15:34 +0800

    Implement selection sort

---
+++ selection/src/main/java/com/saz/sorter/selection/SelectionSorter.java
@@ -0,0 +1,31 @@
+package com.saz.sorter.selection;
+
+import sorter.api.Sorter;
+
+import java.util.Arrays;
+
+public class SelectionSorter implements Sorter {
+
+    @Override
+    public <T extends Comparable<T>> T[] sort(final T[] input) {
+
+        T[] sorted = Arrays.copyOf(input, input.length);
+
+        for (int i = 0; i < sorted.length; ++i) {
+
+            int idxOfSmallest = i;
+            for (int j = i; j < sorted.length; ++j) {
+
+                if (sorted[idxOfSmallest].compareTo(sorted[j]) < 0) {
+                      idxOfSmallest = j;
+                }
+            }
+
+            if (idxOfSmallest != i) {
+                swap(sorted, idxOfSmallest, i);
+            }
+        }
+
+        return sorted;
+    }
+}
