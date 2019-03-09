3736b827fb8e75eba66752828a301d708f06ae32
shafiq <shafiq.akram.zakaria@gmail.com>
2019-03-09 12:43:42 +0800

    Add default method swap.

--- api/src/main/java/sorter/api/Sorter.java
+++ api/src/main/java/sorter/api/Sorter.java
@@ -4,4 +4,11 @@ public interface Sorter {
 
     <T extends Comparable<T>> T[] sort(final T[] input);
 
+    default <T extends Comparable<T>> void swap(T[] input, int idxOne,
+                                                int idxTwo) {
+
+        T temp = input[idxOne];
+        input[idxOne] = input[idxTwo];
+        input[idxTwo] = temp;
+    }
 }
