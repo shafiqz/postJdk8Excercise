914dc9ce59f3c59b9744295774cc97cec427e528
shafiq <shafiq.akram.zakaria@gmail.com>
2019-03-10 16:05:13 +0800

    Move BubbleSorter to com.saz.sorter.bubble

--- bubble/src/main/java/module-info.java
+++ bubble/src/main/java/module-info.java
@@ -3,6 +3,6 @@ module jfx.sorter.selection {
     requires jfx.sorter.api;
     uses com.saz.sorter.api.Sorter;
     uses com.saz.sorter.api.SorterProvider;
-    provides com.saz.sorter.api.Sorter with com.saz.sorter.selection.SelectionSorter;
+    provides com.saz.sorter.api.Sorter with com.saz.sorter.bubble.BubbleSorter;
     //provides com.saz.sorter.api.SorterProvider;
 }
